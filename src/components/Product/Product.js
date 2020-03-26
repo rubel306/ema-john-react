ad3909283667852927fbb55edf1f746845fc4e7a
RubelHasan <rubelhasan306@gmail.com>
2020-03-26 17:15:41 +0600

    Product Component created with dynamic product data

---
+++ src/components/Product/Product.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+    const{name, price, img, stock, seller} = props.product;
+    return (
+        <div className="product">
+           <div className="product-img">
+                <img src={img} alt=""/>
+           </div>
+           <div className="product-details">
+                <h4 className="product-title">
+                    {name}
+                </h4>
+                <p><small>by: {seller}</small></p>
+                <p className="product-price">
+                    ${price}
+                </p>
+                <p className="stock">
+                    only {stock} left in stock - order soon
+                </p>
+                <button className="cartBtn">Add to cart</button>
+           </div>
+        </div>
+    );
+};
+
+export default Product;
